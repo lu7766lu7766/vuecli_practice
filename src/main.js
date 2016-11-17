65571ac38bf3b3b27ac371b3c788cc948754de2b
lu7766lu7766 <lu7766lu7766@gmail.com>
2016-11-17 12:00:54 +0800

    render jsx practice

--- src/main.js
+++ src/main.js
@@ -4,9 +4,37 @@ import router from './router'
 import store from './store'
 
 /* eslint-disable no-new */
-new Vue({
+let vm = new Vue({
   el: '#app',
-  template: '<App />',
+  data: {
+    show: true,
+    message: 'test123'
+  },
+  methods: {
+    chgShow: (e) => {
+      vm.show = !vm.show
+      console.log(vm.show)
+      console.log(e)
+    }
+  },
+  render (h) {
+    if (this.show) {
+      // console.log(this.show)
+      return (
+        <div>
+          hello, world!
+          <button on-click={this.chgShow}>test</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <button on-click={this.chgShow}>test</button>
+          <App />
+        </div>
+      )
+    }
+  },
   components: { App },
   router,
   store
