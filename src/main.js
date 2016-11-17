1e71f05394481138022ca4b02d9baea79d1ba847
lu7766lu7766 <lu7766lu7766@gmail.com>
2016-11-17 15:07:12 +0800

    render jsx change to template

--- src/main.js
+++ src/main.js
@@ -14,29 +14,42 @@ new Vue({
     // chgShow : () =>{} == chgShow : function chgShow(){} error
     chgShow () { // chgShow:function(){} success
       // console.log(this)
-      // this.show = !this.show
+      this.show = !this.show
       this.message++
     }
   },
-  render (h) {
-    if (this.show) {
-      // console.log(this.show)
-      return (
-        <div>
-          hello, world!{this.message}
-          <input type='text' value={this.message}/>
-          <button on-click={this.chgShow.bind(this)}>test</button>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <button on-click={this.chgShow}>test</button>
-          <App />
-        </div>
-      )
-    }
-  },
+  template: `
+    <div>
+      <div v-if="show">
+        hello, world!{{this.message}}<br>
+      </div>
+      <div v-else>
+        bad world!!{{this.message}}<br>
+      </div>
+      <input type='text' v-model="message"/><br>
+      <button @click="chgShow">test</button>
+      <App />
+    </div>
+  `,
+  // render (h) {
+  //   if (this.show) {
+  //     // console.log(this.show)
+  //     return (
+  //       <div>
+  //         hello, world!{this.message}
+  //         <input type='text' value={this.message}/>
+  //         <button on-click={this.chgShow.bind(this)}>test</button>
+  //       </div>
+  //     )
+  //   } else {
+  //     return (
+  //       <div>
+  //         <button on-click={this.chgShow}>test</button>
+  //         <App />
+  //       </div>
+  //     )
+  //   }
+  // },
   components: { App },
   router,
   store
